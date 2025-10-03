29724760b0e854c7ac9870c1ea0ba92f277e6ff5
Hanif.Emamgholizadeh <h.emamgholizadeh@gmail.com>
2025-10-03 08:42:55 +0200

    move zeros

---
+++ array/move_zeros.py
@@ -0,0 +1,19 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        first_zero = -1
+        lst_zero = -1
+        ind = 0
+        nums_length = len(nums)
+        while first_zero < 0 and ind < nums_length:
+            if nums[ind] == 0:
+                first_zero = ind
+                lst_zero = ind
+            ind += 1
+        for i in nums[ind:nums_length]:
+            if i != 0:
+                lst_zero += 1
+                nums[first_zero] = i
+                nums[lst_zero] = 0
+                first_zero += 1
+            else:
+                lst_zero += 1
