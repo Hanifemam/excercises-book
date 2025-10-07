0bad2f83394c97e5518fa2e9a95c3dda157453e1
Hanif.Emamgholizadeh <h.emamgholizadeh@gmail.com>
2025-10-07 08:33:03 +0200

    Iteration added

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -10,9 +10,24 @@ class LinkedList:
         self.tail = node
 
     def prepend(self, value):
-        next = self.head
-        node = Node(value, next)
+        next_node = self.head
+        node = Node(value, next_node)
         self.head = node
+
+    def insert(self, index, value):
+        pass
+
+    def __iter__(self):
+        self._cursor = self.head
+        return self
+
+    def __next__(self):
+        if self._cursor:
+            value = self._cursor.value
+            self._cursor = self._cursor.next
+            return value
+        else:
+            raise StopIteration
 
 
 class Node:
@@ -35,3 +50,31 @@ class Node:
     @value.setter
     def value(self, value):
         self._value = value
+
+
+if __name__ == "__main__":
+    print("Creating linked list with head = 10...")
+    ll = LinkedList(10)
+
+    print("\nAppending values 20 and 30...")
+    ll.append(20)
+    ll.append(30)
+
+    print("Prepending value 5...")
+    ll.prepend(5)
+
+    print("\nIterating through the linked list:")
+    for value in ll:
+        print(value)
+
+    print("\nIterating again (should still work):")
+    for value in ll:
+        print(value)
+
+    print("\nManual iteration using next():")
+    iterator = iter(ll)
+    try:
+        while True:
+            print(next(iterator))
+    except StopIteration:
+        print("Reached end of list.")
