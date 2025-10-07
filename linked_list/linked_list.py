e7391b897d4a4204005fdb1f5b2fb16ac20bff91
Hanif.Emamgholizadeh <h.emamgholizadeh@gmail.com>
2025-10-07 08:03:44 +0200

    LinkedList added

---
+++ linked_list/linked_list.py
@@ -0,0 +1,29 @@
+class LinkedList:
+    def __init__(self, value):
+        node = Node(value, None)
+        self.head = node
+        self.tail = node
+
+    def append(self, value):
+        node = Node(value, next)
+        self.tail.next = node
+        self.tail = node
+
+    def prepend(self, value):
+        next = self.head
+        node = Node(value, next)
+        self.head = node
+
+
+class Node:
+    def __init__(self, value, next):
+        self.value = value
+        self._next = next
+
+    @property
+    def next(self):
+        return self._next
+
+    @next.setter
+    def next(self, next):
+        self._next = next
