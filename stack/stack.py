00c211ceaefefaac9b4e91ad5748a9d6eddfc73f
Hanif.Emamgholizadeh <h.emamgholizadeh@gmail.com>
2025-10-10 07:24:37 +0200

    Stack added

---
+++ stack/stack.py
@@ -0,0 +1,51 @@
+class Stack:
+    def __init__(self):
+        self.top = None
+        self.bottom = None
+        self.length = 0
+
+    def push(self, val):
+        if self.top and self.bottom:
+            node = Node(val)
+            self.bottom = node
+            self.top = node
+        else:
+            node = Node(val, next=self.top)
+            self.length += 1
+            self.top = node
+
+    def pop(self):
+        if self.length == 0:
+            raise ValueError("Stack is empty")
+        if self.top and self.top == self.bottom:
+            val = self.top.val
+            self.top = None
+            self.bottom = None
+            self.length = 0
+            return val
+        else:
+            val = self.top
+            self.top = self.top.next
+            self.length -= 1
+            return val
+
+    def __repr__(self):
+        curr = self.top
+        while curr:
+            print(f"{curr.val} --> ")
+            curr = curr.next
+
+
+class Node:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+# print(stack)
+stack.pop()
+print(stack)
