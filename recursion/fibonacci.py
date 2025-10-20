9c47177bc13a4439326ae94db4de19c604c83977
Hanif.Emamgholizadeh <h.emamgholizadeh@gmail.com>
2025-10-20 08:41:33 +0200

    Fibunaccis updated

--- recursion/fibonacci.py
+++ recursion/fibonacci.py
@@ -1,18 +1,17 @@
 def fibonacci(n):
-    if n < 2:
+    if n <= 2:
         return 1
-    print(n)
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 
-print(fibonacci(7))
+print(fibonacci(8))
 
 
 def fib_iterative(n):
     n_min_two = 0
     n_min_one = 1
-    i = 0
-    while i < n:
+    i = 2
+    while i <= n:
         new_val = n_min_two + n_min_one
         n_min_two = n_min_one
         n_min_one = new_val
@@ -20,4 +19,4 @@ def fib_iterative(n):
     return new_val
 
 
-print(fib_iterative(7))
+print(fib_iterative(8))
