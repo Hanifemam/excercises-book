a28d931354315a8e7b34c493bef2143ef695ad21
Hanif.Emamgholizadeh <h.emamgholizadeh@gmail.com>
2025-10-20 08:29:06 +0200

    Fibonacci with recursion added

--- recursion/fibonacci.py
+++ recursion/fibonacci.py
@@ -1,2 +1,10 @@
-def fibonacci(number1, number2):
-    pass
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    print(n)
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+print(fibonacci(7))
