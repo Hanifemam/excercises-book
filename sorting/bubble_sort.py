449350d50a14c15b7ed53d3bbd468f55d1258370
Hanif.Emamgholizadeh <h.emamgholizadeh@gmail.com>
2025-10-21 08:04:05 +0200

    Bubble sort debugged

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,7 +1,7 @@
 def bubble(arr):
     for i in range(len(arr)):
-        for j in range(i, len(arr), 1):
-            if arr[i] > arr[j]:
+        for j in range(0, len(arr), 1):
+            if arr[i] < arr[j]:
                 temp = arr[i]
                 arr[i] = arr[j]
                 arr[j] = temp
