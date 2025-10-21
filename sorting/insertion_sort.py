7a9c39e558bfad8d3559b793749438275745f481
Hanif.Emamgholizadeh <h.emamgholizadeh@gmail.com>
2025-10-21 08:47:22 +0200

    Insertion sort added

---
+++ sorting/insertion_sort.py
@@ -0,0 +1,16 @@
+def insertion_sort(arr):
+    insertion_arr = []
+    insertion_arr.append(arr[0])
+    for i in range(1, len(arr)):
+        curr = arr[i]
+        for j in range(len(insertion_arr)):
+            if curr < insertion_arr[j]:
+                insertion_arr.insert(j, curr)
+                break
+            if j == len(insertion_arr) - 1:
+                insertion_arr.append(curr)
+
+    return insertion_arr
+
+
+print(insertion_sort([2, 5, 4, 3, 3, 9, 1]))
