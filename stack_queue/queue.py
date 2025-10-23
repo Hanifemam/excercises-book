2f381b426c0cdba966f535761516069df68379d7
Hanif.Emamgholizadeh <h.emamgholizadeh@gmail.com>
2025-10-23 07:56:23 +0200

    BFS added

--- stack_queue/queue.py
+++ stack_queue/queue.py
@@ -11,6 +11,7 @@ class Queue:
             self.last = node
         else:
             node = Node(val, next=self.last)
+            self.last.next = node
             if self.first == self.last:
                 self.first.next = node
             self.last = node
@@ -41,14 +42,14 @@ class Node:
         self.next = next
 
 
-queue = Queue()
-queue.enqueue(3)
-print(queue.peek())
-queue.enqueue(2)
-print(queue.peek())
-queue.enqueue(4)
-print(queue.peek())
-# print(stack)
-print(queue.dequeue())
-print(queue.dequeue())
-print(queue.dequeue())
+# queue = Queue()
+# queue.enqueue(3)
+# print(queue.peek())
+# queue.enqueue(2)
+# print(queue.peek())
+# queue.enqueue(4)
+# print(queue.peek())
+# # print(stack)
+# print(queue.dequeue())
+# print(queue.dequeue())
+# print(queue.dequeue())
--- trees/bfs.py
+++ trees/bfs.py
@@ -1,9 +1,32 @@
+from pathlib import Path
+import sys
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
 from stack_queue.queue import Queue
 
 
 def bfs(tree, root):
     queue = Queue()
     queue.enqueue(root)
-
+    traverse_list = []
     while queue.length > 0:
-        pass
+        next_root = queue.dequeue()
+        traverse_list.append(next_root)
+        for child in tree[next_root]:
+            queue.enqueue(child)
+
+    return traverse_list
+
+
+tree = {
+    "A": ["B", "C", "D"],
+    "B": ["E", "F"],
+    "C": ["G"],
+    "D": [],
+    "E": [],
+    "F": ["H"],
+    "G": [],
+    "H": [],
+}
+root = "A"
+print(bfs(tree, root))
